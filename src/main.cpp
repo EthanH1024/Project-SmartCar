49e58f17770f18442b9e72d29c1beac21997f276
EthanH1024 <0384572@schoolsnet.act.edu.au>
2026-08-20 10:58:54 +1000

    movement part 3

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <vehicle.h>
 #include <ultrasonic.h>
 #include <ESP32Servo.h>
-#include <ultrasonic.h>
 #include <IRremote.h>
 
 /*
@@ -101,26 +100,55 @@ delay(10);
 middleDistance =myUltrasonic.Ranging();
 myServo.write(90);
 if(middleDistance<=25){
-myCar.Move(stop,0);
+myCar.Move(Stop,0);
 myServo.write(0);
 for(int angle =90;angle>=0;angle--){
   myServo.write(angle);
   delay(10);
 }
-}
+
 delay(500);
 rightDistance =myUltrasonic.Ranging();
 for(int angle =0;angle<=180;angle++){
 myServo.write(angle);
 delay(10);
 }
+delay(500);
+leftDistance =myUltrasonic.Ranging();
+if(rightDistance<20&& leftDistance<20){
+  myCar.Move(Backward,180);
+  delay(500);
+  myCar.Move(Contrarotate,180);
+  delay(1000);
+}
+else if(rightDistance< leftDistance){
+  myCar.Move(Backward,180);
+delay(500);
+myCar.Move(Clockwise,180);
+delay(1000);
+}
+else if(rightDistance< leftDistance){
+myCar.Move(Backward,180);
+delay(500);
+myCar.Move(Contrarotate,180);
+  delay(1000);
+}
+else{
+  myCar.Move(Backward,180);
+  delay(500);
+  myCar.Move(Clockwise,180);
+  delay(1000);
+}
+}
+  else{
+    myCar.Move(Forward,150);
+  }
   }
 
 
 
 void setup()
 {
-  Serial.begin(11520);
   pinMode(leftLED, OUTPUT);
   pinMode(rightLED, OUTPUT);
 
