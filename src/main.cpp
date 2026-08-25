58982c85f6e9c9c0f7d71d6022ca59542e81fb64
EthanH1024 <0384572@schoolsnet.act.edu.au>
2026-08-25 12:00:16 +1000

    movement 4

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,6 @@ vehicle myCar;
 Servo myServo;
 ultrasonic myUltrasonic;
 
-
 #define leftLED 2
 #define rightLED 12
 #define buzzer 33
@@ -38,17 +37,19 @@ ultrasonic myUltrasonic;
 IRrecv myIRrecv(IRpin);
 
 const int fadedelay = 10;
-int UT_distance =0; 
-int myServo_PIN =27;
-int leftDistance=0;
-int middleDistance =0;
-int rightDistance =0;
+int UT_distance = 0;
 
-void ledLights() {{
+int leftDistance = 0;
+int middleDistance = 0;
+int rightDistance = 0;
+
+void ledLights()
+{
+  {
     for (int brightness = 0; brightness <= 255; brightness++)
     {
       digitalWrite(leftLED, HIGH);
-     digitalWrite(rightLED, LOW);
+      digitalWrite(rightLED, LOW);
       analogWrite(leftLED, brightness);
       analogWrite(rightLED, brightness);
       delay(fadedelay);
@@ -56,13 +57,11 @@ void ledLights() {{
   }
   delay(1000);
 
-  
- 
   {
     for (int brightness = 255; brightness >= 0; brightness--)
     {
       digitalWrite(leftLED, LOW);
-  digitalWrite(rightLED, HIGH);
+      digitalWrite(rightLED, HIGH);
       analogWrite(leftLED, brightness);
       analogWrite(rightLED, brightness);
       delay(fadedelay);
@@ -71,40 +70,63 @@ void ledLights() {{
   delay(1000);
 }
 
+void objectAdvoidise()
+{
+  myServo.write(90);
+  middleDistance = myUltrasonic.Ranging();
+  Serial.println(middleDistance);
+  if (middleDistance <= 25)
+  {
+    myCar.Move(Stop, 0);
 
-void ultrasonicMovement(){
-UT_distance = myUltrasonic.Ranging();
-  if(UT_distance<= 25)
-  { myCar.Move(Contrarotate,180);
-  delay(1500);
-  myCar.Move(Stop,0);
-  }
-  else{
-myCar.Move(Forward,150);
-  }
-  Serial.print(UT_distance);
-  Serial.println("cm");
-  delay(1000);
+    // Look right
+    myServo.write(0);
+    delay(500);
+    rightDistance = myUltrasonic.Ranging();
 
- /* for(int angle =0;angle<=180;angle++){
-    myServo.write(angle);
-    delay(10);
+    // Look left
+    myServo.write(180);
+    delay(500);
+    leftDistance = myUltrasonic.Ranging();
+
+    // Reset servo to center
+    myServo.write(90);
+
+    // Decide direction based on clear space
+    if (rightDistance < 20 && leftDistance < 20)
+    {
+      myCar.Move(Backward, 180);
+      delay(500);
+      myCar.Move(Contrarotate, 180);
+      delay(1000);
+    }
+    else if (rightDistance > leftDistance)
+    {
+      // Right side has more room
+      myCar.Move(Clockwise, 180);
+      delay(800);
+    }
+    else
+    {
+      // Left side has more room (or equal)
+      myCar.Move(Contrarotate, 180);
+      delay(800);
+    }
   }
-  for(int angle =180;angle>=0;angle--){
-myServo.write(angle);
-delay(10);
-  }*/
-   }
-  
-  void objectAdvoidise(){
-middleDistance =myUltrasonic.Ranging();
+  else
+  {
+    myCar.Move(Forward, 150);
+  }
+}
+
+/*middleDistance =myUltrasonic.Ranging();
 myServo.write(90);
 if(middleDistance<=25){
 myCar.Move(Stop,0);
 myServo.write(0);
 for(int angle =90;angle>=0;angle--){
-  myServo.write(angle);
-  delay(10);
+myServo.write(angle);
+delay(10);
 }
 
 delay(500);
@@ -116,13 +138,13 @@ delay(10);
 delay(500);
 leftDistance =myUltrasonic.Ranging();
 if(rightDistance<20&& leftDistance<20){
-  myCar.Move(Backward,180);
-  delay(500);
-  myCar.Move(Contrarotate,180);
-  delay(1000);
+myCar.Move(Backward,180);
+delay(500);
+myCar.Move(Contrarotate,180);
+delay(1000);
 }
 else if(rightDistance< leftDistance){
-  myCar.Move(Backward,180);
+myCar.Move(Backward,180);
 delay(500);
 myCar.Move(Clockwise,180);
 delay(1000);
@@ -131,21 +153,19 @@ else if(rightDistance< leftDistance){
 myCar.Move(Backward,180);
 delay(500);
 myCar.Move(Contrarotate,180);
-  delay(1000);
+delay(1000);
 }
 else{
-  myCar.Move(Backward,180);
-  delay(500);
-  myCar.Move(Clockwise,180);
-  delay(1000);
+myCar.Move(Backward,180);
+delay(500);
+myCar.Move(Clockwise,180);
+delay(1000);
 }
 }
-  else{
-    myCar.Move(Forward,150);
-  }
-  }
-
-
+else{
+myCar.Move(Forward,150);
+}
+}*/
 
 void setup()
 {
@@ -155,26 +175,18 @@ void setup()
   analogWrite(leftLED, 50);
   analogWrite(rightLED, 50);
 
-  myServo.write(0);
   myServo.attach(servoPin);
+  myServo.write(0);
   myCar.Init();
 
-  Serial.begin(9600);
-  myUltrasonic.Init(13,14);
-  myServo.attach(myServo_PIN);//initialize servo motor
-  myServo.write(0);
-
-  myServo.attach(myServo_PIN);
+  Serial.begin(115200);
+  myUltrasonic.Init(13, 14);
 }
 
 void loop()
 { // put your main code here, to run repeatedly:
-  ledLights();
-  ultrasonicMovement();
-objectAdvoidise();
-
+  // ledLights();
+  objectAdvoidise();
 
   delay(100);
 }
- 
-   
