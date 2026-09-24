b6c7417c199d6467bad3044fc2219263ee00516b
EthanH1024 <0384572@schoolsnet.act.edu.au>
2026-09-24 13:22:41 +1000

    movment with the remote

--- src/main.cpp
+++ src/main.cpp
@@ -29,16 +29,22 @@ vehicle myCar;
 Servo myServo;
 ultrasonic myUltrasonic;
 
-const int RECV_PIN = 4; // Check your specific ESP32 pin in your manual (e.g., 19 or 32)
-bool obstacleAvoidanceEnabled = false; // Toggle state variable 
-
+const int RECV_PIN = 33;               // Check your specific ESP32 pin in your manual (e.g., 19 or 32)
+bool obstacleAvoidanceEnabled = false; // Toggle state variable
 
 #define leftLED 2
 #define rightLED 12
 #define buzzer 33
 #define servoPin 25
-#define IRpin 4
+#define iRpin 33
 
+unsigned long lastCommandTime;
+const unsigned long commandTimeout = 100;
+uint32_t last_decode = 0;
+uint32_t current_decode = 0;
+
+IRrecv myiRrecv(iRpin);
+int Speed = 255;
 
 const int fadedelay = 10;
 int UT_distance = 0;
@@ -47,32 +53,46 @@ int leftDistance = 0;
 int middleDistance = 0;
 int rightDistance = 0;
 
-/*void ledLights() {
+void remoteMovement()
+{
+  if (myiRrecv.decode())
   {
-    for (int brightness = 0; brightness <= 255; brightness++)
+    lastCommandTime = millis();
+    current_decode = myiRrecv.decodedIRData.decodedRawData;
+    if (myiRrecv.decodedIRData.flags)
     {
-      digitalWrite(leftLED, HIGH);
-      digitalWrite(rightLED, LOW);
-      analogWrite(leftLED, brightness);
-      analogWrite(rightLED, brightness);
-      delay(fadedelay);
+      current_decode = last_decode;
     }
+    Serial.print(current_decode, HEX);
+    Serial.println("");
+    switch (current_decode)
+    {
+    case 0xB946FF00:
+      myCar.Move(Forward, Speed);
+      break;
+    case 0xEA15FF00:
+      myCar.Move(Backward, Speed);
+      break;
+    case 0xBB44FF00:
+      myCar.Move(Clockwise, Speed);
+      break;
+    case 0xBC43FF00:
+      myCar.Move(Move_Left, Speed);
+      break;
+    case 0xF20DFF00:
+      myCar.Move(Move_Right, Speed);
+      break;
+    }
+    last_decode = current_decode;
+    myiRrecv.resume();
   }
-  delay(1000);
+  if (millis() - lastCommandTime > commandTimeout)
+  {
+    myCar.Move(Stop, 0);
+  }
+}
 
-  {
-    for (int brightness = 255; brightness >= 0; brightness--)
-    {
-      digitalWrite(leftLED, LOW);
-      digitalWrite(rightLED, HIGH);
-      analogWrite(leftLED, brightness);
-      analogWrite(rightLED, brightness);
-      delay(fadedelay);
-    }
-  }
-  delay(1000);
-}*/
-void objectAdvoidise()
+/*void objectAdvoidise()
 {
   myServo.write(90);
   middleDistance = myUltrasonic.Ranging();
@@ -82,7 +102,7 @@ void objectAdvoidise()
     digitalWrite(leftLED, HIGH);
     digitalWrite(rightLED, HIGH);
     myCar.Move(Stop, 0);
- 
+
 
     // Look right
     myServo.write(0);
@@ -102,8 +122,8 @@ digitalWrite(leftLED, LOW);
     // Decide direction based on clear space
     if (rightDistance < 20 && leftDistance < 20)
     {
-    
-     
+
+
       myCar.Move(Backward, 180);
        for (int i = 0; i < 2; i++) {
         digitalWrite(leftLED, HIGH);
@@ -126,7 +146,7 @@ digitalWrite(leftLED, LOW);
      myCar.Move(Contrarotate, 180);
       delay(1000);
      digitalWrite(leftLED, LOW);
-      digitalWrite(rightLED, LOW);  
+      digitalWrite(rightLED, LOW);
 
     }
     else if (rightDistance > leftDistance)
@@ -146,7 +166,7 @@ digitalWrite(leftLED, LOW);
       myCar.Move(Contrarotate, 180);
       delay(800);
       digitalWrite(leftLED, LOW); // Turn off when finished
-   
+
     }
   }
   else
@@ -160,23 +180,30 @@ void remoteChange(){
   // Check if an IR signal is received
   if (IrReceiver.decode()){
     unsigned long irValue = IrReceiver.decodedIRData.decodedRawData;
-    
+
     // Print hex code to serial monitor for verification
     Serial.println(irValue, HEX);
 
     // Check if the OK button (bf40ff00) is pressed
     if (irValue == 0xBF40FF00) {
       obstacleAvoidanceEnabled = !obstacleAvoidanceEnabled; // Toggle ON/OFF state
-      
+
       if (!obstacleAvoidanceEnabled) {
-        myCar.Move(Stop, 0); // Stop the car immediately when turned off
+         Serial.println("MODE: Object Avoidance Enabled");
       }
+      else {
+        Serial.println("MODE: Remote Control Enabled");
+          myCar.Move(Stop, 0); // Stop the car immediately when turned off
+      }
+
+     if (!obstacleAvoidanceEnabled) {
+       // example: if (irValue == 0x...) { myCar.Move(Forward, 255); }
+    }
+
 }}
 IrReceiver.resume(); // CRITICAL: Receive the next value
 
-}
-  
-
+}*/
 
 void setup()
 {
@@ -193,19 +220,19 @@ void setup()
   Serial.begin(115200);
   myUltrasonic.Init(13, 14);
 
-   IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK);
-  }
-  
-
+  //  IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK);
+  myiRrecv.enableIRIn();
+}
 
 void loop()
- {
+{
   // put your main code here, to run repeatedly:
   // ledLights();
-  objectAdvoidise();
+  /*objectAdvoidise();
   remoteChange();
   if (obstacleAvoidanceEnabled) {
-    
-  }
+
+  }*/
+  remoteMovement();
   delay(100);
- }
+}
